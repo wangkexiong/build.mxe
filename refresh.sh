3af07ff3f70c11827ad103359fd6f3126e1f2cb4
wangkexiong <wangkexiong@gmail.com>
2019-01-29 14:55:28 +0800

    less github API usage

--- refresh.sh
+++ refresh.sh
@@ -5,20 +5,31 @@ if [ "x$GITHUB_TOKEN" == "x" ]; then
 else
   TIME=`date -R`
 
+  git clone -b $TRAVIS_BRANCH https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG refresh_repo
+  pushd refresh_repo
+  sed -i "s/\*\*$MXE_TARGETS Last Build in:.*/\*\*$MXE_TARGETS Last Build in: $TIME\*\*/g" README.md
+  git add README.md
+  git commit --amend --date="$TIME" -m "daily build cc.core..."
+  git push --force
+  popd
+  rm -rf refresh_repo
+
+  # Check if update meets error
   for i in `seq 5`
   do
-    XYZ=`curl -k -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/contents/README.md?ref=$TRAVIS_BRANCH"`
-    REF=`echo $XYZ | jq -r .content | base64 -d - | grep $MXE_TARGETS | tr -d \* | gawk -F ": " '{print $2}'`
+    DUR=`shuf -i5-15 -n1`
+    echo "Sleep $DUR seconds..."
+    sleep $DUR
 
+    git clone -b $TRAVIS_BRANCH https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG refresh_repo
+
+    pushd refresh_repo
+    REF=`cat README.md | grep $MXE_TARGETS | tr -d \* | gawk -F ": " '{print $2}'`
     if [ "$TIME" == "$REF" ]; then
+      popd
+      rm -rf refresh_repo
       break
     else
-      DUR=`shuf -i5-15 -n1`
-      echo "Sleep $DUR seconds..."
-      sleep $DUR
-
-      git clone -b $TRAVIS_BRANCH https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG refresh_repo
-      pushd refresh_repo
       sed -i "s/\*\*$MXE_TARGETS Last Build in:.*/\*\*$MXE_TARGETS Last Build in: $TIME\*\*/g" README.md
       git add README.md
       git commit --amend --date="$TIME" -m "daily build cc.core..."
